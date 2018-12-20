278e9580cf67eb248a8b9d7ea57fae0cd457a602
karmab <karimboumedhel@gmail.com>
2018-12-20 11:20:18 +0100

    origin plan wildcard

--- openshift/istio.sh
+++ openshift/istio.sh
@@ -1,3 +1,9 @@
+curl -L https://github.com/istio/istio/releases/download/1.0.3/istio-1.0.3-linux.tar.gz | tar xz
+cd istio-1.0.3
+export ISTIO_HOME=`pwd`
+export PATH=$ISTIO_HOME/bin:$PATH
+oc apply -f install/kubernetes/helm/istio/templates/crds.yaml
+oc create ns istio-system
 oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system
 oc adm policy add-scc-to-user anyuid -z default -n istio-system
 oc adm policy add-scc-to-user anyuid -z prometheus -n istio-system
@@ -10,7 +16,14 @@ oc adm policy add-scc-to-user anyuid -z istio-mixer-service-account -n istio-sys
 oc adm policy add-scc-to-user anyuid -z istio-pilot-service-account -n istio-system
 oc adm policy add-scc-to-user anyuid -z istio-sidecar-injector-service-account -n istio-system
 oc adm policy add-cluster-role-to-user cluster-admin -z istio-galley-service-account -n istio-system
-curl -L https://storage.googleapis.com/knative-releases/serving/latest/istio.yaml | sed 's/LoadBalancer/NodePort/' | oc apply -f -
+#curl -L https://storage.googleapis.com/knative-releases/serving/latest/istio.yaml | sed 's/LoadBalancer/NodePort/' | oc apply -f -
 oc adm policy add-scc-to-user privileged -z default -n default
 oc label namespace default istio-injection=enabled
+oc apply -f install/kubernetes/istio-demo.yaml
+oc project istio-system
+oc expose svc istio-ingressgateway
+oc expose svc servicegraph
+oc expose svc grafana
+oc expose svc prometheus
+oc expose svc tracing
 sh /root/istio_fix_sidecar.sh
