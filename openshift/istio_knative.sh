751568065ee615fa48554cd0e57f26256c982061
karmab <karimboumedhel@gmail.com>
2018-12-20 20:03:12 +0100

    openshift/origin: reboot with istio

--- openshift/istio_knative.sh
+++ openshift/istio_knative.sh
@@ -28,3 +28,15 @@ oc expose svc servicegraph
 oc expose svc grafana
 oc expose svc prometheus
 oc expose svc tracing
+{% if knative %}
+oc adm policy add-scc-to-user anyuid -z build-controller -n knative-build
+oc adm policy add-scc-to-user anyuid -z controller -n knative-serving
+oc adm policy add-scc-to-user anyuid -z autoscaler -n knative-serving
+oc adm policy add-scc-to-user anyuid -z kube-state-metrics -n knative-monitoring
+oc adm policy add-scc-to-user anyuid -z node-exporter -n knative-monitoring
+oc adm policy add-scc-to-user anyuid -z prometheus-system -n knative-monitoring
+oc adm policy add-cluster-role-to-user cluster-admin -z build-controller -n knative-build
+oc adm policy add-cluster-role-to-user cluster-admin -z controller -n knative-serving
+curl -L https://storage.googleapis.com/knative-releases/serving/latest/release-lite.yaml | sed 's/LoadBalancer/NodePort/' | oc apply -f -
+{% endif %}
+reboot
