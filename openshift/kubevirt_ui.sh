945f59b716d71fc519e28ac5748b4bf150a15a77
karmab <karimboumedhel@gmail.com>
2019-01-09 10:13:28 +0100

    name kubevirt ui route web

--- openshift/kubevirt_ui.sh
+++ openshift/kubevirt_ui.sh
@@ -1,3 +1,2 @@
-oc new-project kweb-ui
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:kweb-ui:default
-oc apply -f kubevirt_ui.yml
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:kubevirt:default
+oc apply -f kubevirt_ui.yml -n kubevirt
