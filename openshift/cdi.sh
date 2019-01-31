479ce226c3aac3cbf4124f45c1abe7810a7e4ded
karmab <karimboumedhel@gmail.com>
2019-01-31 16:26:38 +0100

    update kubevirt and cdi deployments to use their operators

--- openshift/cdi.sh
+++ openshift/cdi.sh
@@ -2,9 +2,8 @@ VERSION="{{ cdi_version }}"
 if [ "$VERSION" == "latest" ] ; then
 VERSION=`curl -s https://api.github.com/repos/kubevirt/containerized-data-importer/releases/latest | jq -r .tag_name`
 fi
-oc new-project cdi
-oc adm policy add-scc-to-user privileged system:serviceaccount:cdi:default
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:cdi:cdi-apiserver
-wget -P /root/ https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-controller.yaml
-oc create -f /root/cdi-controller.yaml
-oc expose svc cdi-uploadproxy -n golden
+wget https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+wget https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator-cr.yaml
+oc create -f cdi-operator.yaml
+oc create -f cdi-operator-cr.yaml
+oc expose svc cdi-uploadproxy -n cdi
--- openshift/kubevirt.sh
+++ openshift/kubevirt.sh
@@ -4,13 +4,11 @@ yum -y install xorg-x11-xauth virt-viewer
 if [ "$VERSION" == "latest" ] ; then
 VERSION=`curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | jq -r .tag_name`
 fi
-oc project $NS
+oc create $NS
 grep -q vmx /proc/cpuinfo || oc create configmap -n $NS kubevirt-config --from-literal debug.useEmulation=true
-wget https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt.yaml
-oc adm policy add-scc-to-user privileged -z kubevirt-privileged -n $NS
-oc adm policy add-scc-to-user privileged -z kubevirt-controller -n $NS
-oc adm policy add-scc-to-user privileged -z kubevirt-apiserver -n $NS
-oc create -f kubevirt.yaml --validate=false
+wget https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml
+wget https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml
+oc create -f kubevirt-operator.yaml
 wget https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-linux-amd64
 mv virtctl-${VERSION}-linux-amd64 /usr/bin/virtctl
 chmod u+x /usr/bin/virtctl
@@ -19,3 +17,4 @@ echo alias kcli=\'docker run --security-opt label:disable -it --rm -v ~/.kube:/r
 ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa
 oc login --insecure-skip-tls-verify=true  `hostname -I | cut -f1 -d' '`:8443 -u {{ admin_user  }} -p {{ admin_password }}
 setfacl -m user:107:rwx /root/openshift.local.clusterup/openshift.local.pv/pv*
+oc create -f kubevirt-cr.yaml
