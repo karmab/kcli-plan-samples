b31be2c1cffa0f0a1da9cdb504841cc401a6167e
karmab <karimboumedhel@gmail.com>
2019-03-06 12:35:59 +0100

    ceph plan

---
+++ ceph/deploy.sh
@@ -0,0 +1,17 @@
+yum -y install git wget
+{% if branch != "master" %}
+yum -y install  https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.9-1.el7.ans.noarch.rpm
+{% else %}
+yum -y install epel-release
+yum -y install ansible
+{% endif %}
+mkdir /root/ceph-ansible-keys
+sed -i "s/#host_key_checking/host_key_checking = True/" /etc/ansible/ansible.cfg
+sed -i "s/#log_path/log_path/" /etc/ansible/ansible.cfg
+cd /root
+git clone https://github.com/ceph/ceph-ansible.git
+cd ceph-ansible
+git checkout {{ branch }}
+cp site.yml.sample site.yml
+cp /root/all.yml group_vars
+#ansible-playbook -i /root/inventory site.yml
