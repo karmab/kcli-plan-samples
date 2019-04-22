769bdda8706403be4a1a230edbdd6197d0c36fa0
karmab <karimboumedhel@gmail.com>
2019-04-22 20:13:06 +0200

    allow knative functions to reach the outside

--- openshift/knative.sh
+++ openshift/knative.sh
@@ -7,3 +7,5 @@ oc adm policy add-scc-to-user anyuid -z prometheus-system -n knative-monitoring
 oc adm policy add-cluster-role-to-user cluster-admin -z build-controller -n knative-build
 oc adm policy add-cluster-role-to-user cluster-admin -z controller -n knative-serving
 curl -L https://storage.googleapis.com/knative-releases/serving/latest/serving.yaml | sed 's/LoadBalancer/NodePort/' | oc apply --filename -
+#oc get cm config-network -n knative-serving -o yaml | sed  's@istio.sidecar.includeOutboundIPRanges:.*@istio.sidecar.includeOutboundIPRanges: "10.0.0.1/24"@'  | oc replace -f -
+oc get cm config-network -n knative-serving -o yaml | sed  's@istio.sidecar.includeOutboundIPRanges:.*@istio.sidecar.includeOutboundIPRanges: "172.17.0.0/16"@'  | oc replace -f -
