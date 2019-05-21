4270d3f8a7cab001e95300bbbccc9239d2e2ec4e
karmab <karimboumedhel@gmail.com>
2019-05-21 19:49:47 +0200

    try to use ip for append config

--- openshift/upi/dnsmasq.sh
+++ openshift/upi/dnsmasq.sh
@@ -1,7 +1,6 @@
 yum -y install dnsmasq
 echo address=/apps.{{ cluster }}.{{ domain }}/{{ haproxy_ip }} >> /etc/dnsmasq.conf
 {% for num in range(0, masters) %}
-echo {{ masters_ips[num] }} {{ prefix }}-master-{{ num }} {{ prefix }}-master-{{ num }}.{{ cluster}}.{{ domain }} >> /etc/dnsmasq.conf
 echo host-record={{ prefix }}-master-{{ num }}.{{ cluster }}.{{ domain }},etcd-{{ num }}.{{ cluster }}.{{ domain }},{{ masters_ips[num ] }},3600 >> /etc/dnsmasq.conf
 echo srv-host=_etcd-server-ssl._tcp.{{ cluster }}.{{ domain }},etcd-{{ num }}.{{ cluster }}.{{ domain }},2380,0,10 >> /etc/dnsmasq.conf
 {% endfor %}
