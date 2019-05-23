627e2b1403727781de02ad9c952366c291b8dc9c
karmab <karimboumedhel@gmail.com>
2019-05-23 22:54:22 +0200

    handle bridge in upi

--- openshift/upi/kcli_ocp.sh
+++ openshift/upi/kcli_ocp.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-masters=1
-workers=0
-cluster=testk
-domain=karmalabs.com
-prefix=karim
+
+. env.sh || true
+prefix="${prefix:-karim}"
+numcpus="${numcpus:-4}"
+network="${network:-default}"
+use_br="${use_br:-false}"
+extra_disk="${extra_disk:-false}"
+master_memory="${master_memory:-8192}"
+worker_memory="${worker_memory:-8192}"
+bootstrap_memory="${bootstrap_memory:-4096}"
+haproxy_memory="${haproxy_memory:-2048}"
+disk_size="${disk_size:-30}"
+extra_disk_size="${extra_disk_size:-10}"
+template="${template:-rhcos-410.8.20190520.1-qemu.qcow2}"
+haproxy_template="${haproxy_template:-CentOS-7-x86_64-GenericCloud.qcow2}"
+cluster="${cluster:-testk}"
+domain="${domain:-karmalabs.com}"
+masters="${masters:-1}"
+workers="${workers:-0}"
 
 pubkey=`cat ~/.ssh/id_rsa.pub`
 pullsecret=`cat openshift_pull.json`
@@ -16,7 +30,7 @@ sed -i "s%PULLSECRET%$pullsecret%" $cluster/install-config.yaml
 sed -i "s%PUBKEY%$pubkey%" $cluster/install-config.yaml
 openshift-install --dir $cluster create ignition-configs
 
-kcli plan -f kcli_ocp_temp.yml -P prefix=$prefix -P masters=$masters -P workers=$workers temp_$prefix
+kcli plan -f kcli_ocp_temp.yml -P prefix=$prefix -P masters=$masters -P workers=$workers -P network=$network temp_$prefix
 
 all="$prefix-haproxy $prefix-bootstrap"
 for i in `seq 0 $masters` ; do 
@@ -32,11 +46,13 @@ done
 
 total=$(( $(echo $all | wc -w)  * 2 ))
 current=0
+BLUE='\033[0;36m'
+NC='\033[0m'
 
 while [ $current != $total ] ; do
     info=$(kcli info -f ip,nets -v $all | sed 's/.*mac: \(.*\) net:.*/\1/')
     current=$(echo $info | wc -w)
-    echo "Waiting 5s to gather ips and macs from nodes..."
+    echo -e "${BLUE}Waiting 5s to gather ips and macs from nodes...${NC}"
     sleep 5
 done
 
@@ -76,6 +92,18 @@ for i in `seq 0 $workers` ; do
 done
 
 echo """prefix: $prefix
+numcpus: $numcpus
+network: $network
+use_br: $use_br
+extra_disk: $extra_disk
+master_memory: $master_memory
+worker_memory: $worker_memory
+bootstrap_memory: $bootstrap_memory
+haproxy_memory: $haproxy_memory
+disk_size: $disk_size
+extra_disk_size: $extra_disk_size
+template: $template
+haproxy_template: $haproxy_template
 cluster: $cluster
 domain: $domain
 masters: $masters
@@ -103,5 +131,6 @@ for entry in `echo $workers_macs` ; do
 done
 
 kcli plan --yes -d  temp_$prefix
-sed -i s@https://api-int.$cluster.$domain::22623/config@http://$haproxy_ip:8080@ $cluster/master.ign $cluster/worker.ign
+sed -i s@https://api-int.$cluster.$domain:22623/config@http://$haproxy_ip:8080@ $cluster/master.ign $cluster/worker.ign
 kcli plan -f kcli_ocp.yml --paramfile $cluster/$prefix.yml $prefix
+export KUBECONFIG=$PWD/$cluster/auth/kubeconfig
