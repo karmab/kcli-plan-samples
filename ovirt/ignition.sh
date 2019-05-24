dac94a6eb26fd132d52235de0078bbbcd7cf1dc4
karmab <karimboumedhel@gmail.com>
2019-05-24 14:03:10 +0200

    updated doc

--- ovirt/ignition.sh
+++ ovirt/ignition.sh
@@ -1,4 +1,4 @@
-engine-config -s 'UserDefinedVMProperties=ignitiondata=.*' --cver='{{ version }}'
+engine-config -s UserDefinedVMProperties='ignitiondata=.*' --cver='{{ version }}'
 systemctl restart ovirt-engine
 cp /root/90_ignition /usr/libexec/vdsm/hooks/before_vm_start
 chmod 755 /usr/libexec/vdsm/hooks/before_vm_start/90_ignition
--- ovirt/install.sh
+++ ovirt/install.sh
@@ -1,4 +1,4 @@
-echo "option kvm_intel nested=1" >> /etc/modprobe.d/kvm-intel.conf
+echo "options kvm_intel nested=1" >> /etc/modprobe.d/kvm-intel.conf
 modprobe -r kvm_intel ; modprobe kvm_intel
 yum -y install ovirt-engine ovirt-engine-cli
 yum -y update selinux-policy
@@ -9,6 +9,5 @@ systemctl start rngd
 engine-setup --config-append=/root/answers.txt
 yum -y install vdsm
 {% if nested %}
-echo "options kvm-intel nested=Y" > /etc/modprobe.d/kvm-intel.conf
 yum -y install vdsm-hook-nestedvt vdsm-hook-macspoof
 {% endif %}
