cb7130b056c576abac440779619c1b23ffcb56a5
karmab <karimboumedhel@gmail.com>
2019-11-18 16:53:21 +0100

    no screen

--- openshift4/baremetal/run.sh
+++ openshift4/baremetal/run.sh
@@ -4,7 +4,7 @@ export PATH=/root:$PATH
 export KUBECONFIG=/root/ocp/auth/kubeconfig
 export OS_CLOUD=metal3-bootstrap
 export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:{{ tag }}
-mkdir ocp
+mkdir -p ocp/openshift
 cp install-config.yaml ocp
-screen -S fix_configmap -dm bash -c "/root/fix_configmap.sh"
+cp metal3-config.yaml ocp/openshift
 openshift-baremetal-install --dir ocp --log-level debug create cluster
