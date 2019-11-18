da5495246148b5b2987590a0f9a24d7b29625842
karmab <karimboumedhel@gmail.com>
2019-11-18 11:16:45 +0100

    fix typo

--- openshift4/baremetal/fix_configmap.sh
+++ openshift4/baremetal/fix_configmap.sh
@@ -1,7 +1,7 @@
 export PATH=/root:$PATH
 export HOME=/root
 export KUBECONFIG=/root/ocp/auth/kubeconfig
-while True ; do
+while true ; do
     oc create -f metal3-config.yaml -n openshift-machine-api
     if [ "$?" == "0" ] ; then
         exit 0
