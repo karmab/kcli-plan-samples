e254ebc665ff6387be1f02ac626fe12dbfaa946b
karmab <karimboumedhel@gmail.com>
2019-11-18 12:22:40 +0100

    exits with python yaml is missing

--- openshift4/baremetal/clean.sh
+++ openshift4/baremetal/clean.sh
@@ -1,7 +1,7 @@
 rm -rf /root/ocp
 export LIBVIRT_DEFAULT_URI=$(grep libvirtURI install-config.yaml | sed 's/libvirtURI: //' | xargs)
 CLUSTER=$(yq r install-config.yaml metadata.name)
-BOOTSTRAP=$(virsh list --name | grep $CLUSTER-bootstrap)
+BOOTSTRAP=$(virsh list --name | grep "$CLUSTER.*bootstrap")
 virsh destroy $BOOTSTRAP
 virsh undefine $BOOTSTRAP
 virsh vol-delete $BOOTSTRAP default
