f6e5e898ca9e4a4918cb12caf7a05b03980c7bef
karmab <karimboumedhel@gmail.com>
2019-11-18 23:09:53 +0100

    configmap bmo

--- openshift4/baremetal/run.sh
+++ openshift4/baremetal/run.sh
@@ -6,5 +6,6 @@ export OS_CLOUD=metal3-bootstrap
 export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:{{ tag }}
 mkdir -p ocp/openshift
 cp install-config.yaml ocp
-cp metal3-config.yaml ocp/openshift
+openshift-baremetal-install --dir ocp --log-level debug create manifests
+cp metal3-config.yaml ocp/openshift/99_metal3-config.yaml
 openshift-baremetal-install --dir ocp --log-level debug create cluster
