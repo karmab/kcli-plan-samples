6d0aab3b9b158e4e46173087cbff42937b770cbb
karmab <karimboumedhel@gmail.com>
2019-11-21 12:06:33 +0100

    make it easy to retrieve latest installer afterwards

--- openshift4/baremetal/baremetal.sh
+++ openshift4/baremetal/baremetal.sh
@@ -19,9 +19,7 @@ export PATH=/root:$PATH
 chmod +x oc
 
 {% if not build %}
-export PULL_SECRET="openshift_pull.json"
-export OPENSHIFT_RELEASE_IMAGE=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp-dev-preview/latest/release.txt | grep 'Pull From: quay.io' | awk -F ' ' '{print $3}' | xargs)
-oc adm release extract --registry-config $PULL_SECRET --command=openshift-baremetal-install --to . $OPENSHIFT_RELEASE_IMAGE
+bash /root/get_installer.sh
 {% endif %}
 
 if [ -z "$COMMIT_ID" ] ; then
--- openshift4/baremetal/clean.sh
+++ openshift4/baremetal/clean.sh
@@ -2,7 +2,9 @@ rm -rf /root/ocp
 export LIBVIRT_DEFAULT_URI=$(grep libvirtURI install-config.yaml | sed 's/libvirtURI: //' | xargs)
 CLUSTER=$(yq r install-config.yaml metadata.name)
 BOOTSTRAP=$(virsh list --name | grep "$CLUSTER.*bootstrap")
+if [ "$BOOTSTRAP" != "" ] ; then
 virsh destroy $BOOTSTRAP
 virsh undefine $BOOTSTRAP
 virsh vol-delete $BOOTSTRAP default
 virsh vol-delete $BOOTSTRAP.ign default
+fi
