faef9cdb9284c10060f2b748fb4cced0b2a2d944
karmab <karimboumedhel@gmail.com>
2019-12-11 08:32:03 +0100

    retrieve tagged version

--- openshift4/baremetal/get_installer.sh
+++ openshift4/baremetal/get_installer.sh
@@ -1,3 +1,3 @@
 export PULL_SECRET="openshift_pull.json"
-export OPENSHIFT_RELEASE_IMAGE=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp-dev-preview/latest/release.txt | grep 'Pull From: quay.io' | awk -F ' ' '{print $3}' | xargs)
+export OPENSHIFT_RELEASE_IMAGE=registry.svc.ci.openshift.org/ocp/release:{{ tag }}
 oc adm release extract --registry-config $PULL_SECRET --command=openshift-baremetal-install --to . $OPENSHIFT_RELEASE_IMAGE
