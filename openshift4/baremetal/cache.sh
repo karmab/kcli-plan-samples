cf9616faeb1700d92a71c9a0fcebf577b6b4983c
karmab <karimboumedhel@gmail.com>
2019-12-20 12:06:05 +0100

    allow cache to happen on provisioning vm

--- openshift4/baremetal/cache.sh
+++ openshift4/baremetal/cache.sh
@@ -9,9 +9,7 @@ export RHCOS_PATH=$(curl -s -S https://raw.githubusercontent.com/openshift/insta
 export RHCOS_SHA_UNCOMPRESSED=$(curl -s -S https://raw.githubusercontent.com/openshift/installer/$COMMIT_ID/data/data/rhcos.json  | jq -r '.images.qemu["uncompressed-sha256"]')
 export RHCOS_SHA_COMPRESSED=$(curl -s -S https://raw.githubusercontent.com/openshift/installer/$COMMIT_ID/data/data/rhcos.json  | jq -r '.images.qemu.sha256')
 curl -L ${RHCOS_PATH}${RHCOS_URI} > $RHCOS_URI
-echo $RHCOS_SHA_UNCOMPRESSED $RHCOS_URI > $SHORT_URI.sha256sum
-#sed -i "/apiVIP/i\ \ \ \ bootstrapOSImage: http://{{ provisioning_ip }}/$RHCOS_URI?sha256=$RHCOS_SHA_UNCOMPRESSED" /root/install-config.yaml
-#sed -i "/apiVIP/i\ \ \ \ clusterOSImage: http://{{ provisioning_ip }}/$RHCOS_URI?sha256=$RHCOS_SHA_COMPRESSED" /root/install-config.yaml
+echo $RHCOS_SHA_UNCOMPRESSED $RHCOS_URI > $RHCOS_URI.sha256sum
 SPACES=$(grep apiVIP /root/install-config.yaml | sed 's/apiVIP.*//' | sed 's/ /\\ /'g)
 sed -i "/apiVIP/i${SPACES}bootstrapOSImage: http://{{ provisioning_ip }}/$RHCOS_URI?sha256=$RHCOS_SHA_UNCOMPRESSED" /root/install-config.yaml
 sed -i "/apiVIP/i${SPACES}clusterOSImage: http://{{ provisioning_ip }}/$RHCOS_URI?sha256=$RHCOS_SHA_COMPRESSED" /root/install-config.yaml
