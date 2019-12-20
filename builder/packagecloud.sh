947dfa8d7cc1d5be3471f51b63a86efa2af88cd7
karmab <karimboumedhel@gmail.com>
2019-12-20 17:53:27 +0100

    update packagecloud builder for rolling release

--- builder/packagecloud.sh
+++ builder/packagecloud.sh
@@ -1,13 +1,12 @@
-export HOME=/root
-export VERSION=`grep Version: /root/package.spec  | cut -d":" -f2 | xargs`
-export MINOR=`grep Release: /root/package.spec  | cut -d":" -f2 | xargs`
+export HOME=.
+export VERSION="99.0"
+export MINOR="$(git rev-parse --short HEAD)"
 apt-get update 
-apt-get -y install python3-stdeb ruby ruby-dev wget python-all
+apt-get -y install python3-stdeb ruby ruby-dev python-all
 gem install rake package_cloud
 export DEB_BUILD_OPTIONS=nocheck debuild
-wget https://pypi.python.org/packages/source/k/kcli/kcli-$VERSION.tar.gz
-tar xzf kcli-$VERSION.tar.gz
-cd kcli-$VERSION
+git clone https://github.com/karmab/kcli
+cd kcli
 find . -name *pyc -exec rm {} \;
 python3 setup.py --command-packages=stdeb.command sdist_dsc --debian-version $MINOR --depends python3-dateutil,python3-prettytable,python3-flask,python3-netaddr,python3-libvirt,genisoimage bdist_deb
 /usr/local/bin/package_cloud push karmab/kcli/ubuntu/xenial deb_dist/*deb
