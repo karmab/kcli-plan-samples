cd81b637118067c7e051fae08a43ea93c512751a
karmab <karimboumedhel@gmail.com>
2020-01-21 23:12:24 +0100

    updated deploy cnf script

--- openshift4/baremetal/cnf/deploy.sh
+++ openshift4/baremetal/cnf/deploy.sh
@@ -1,16 +1,34 @@
 
 FEATURES_DIR="validation"
-# tag nodes
+export FEATURES="performance ptp sctp sriov"
+
+# TAGGING
+MASTER_NODES=$(oc get nodes --selector='node-role.kubernetes.io/master' -o name)
 WORKER_NODES=$(oc get nodes --selector='node-role.kubernetes.io/worker' -o name)
-node=$(echo "$WORKER_NODES" | wc -l)
-sctpnode=$(echo "$WORKER_NODES" | head -1)
-oc label $sctpnode node-role.kubernetes.io/worker-sctp=""
+# tag first master for ptp
+FIRST_MASTER=$(echo $MASTER_NODES | head -1)
+oc label $FIRST_MASTER ptp/grandmaster=''
+# tag workers for ptp
+for node in $WORKER_NODES ; do 
+  oc label $node ptp/slave=''
+done
+
+# tag all workers but last as worker-rt
+sctpnode=$(echo "$WORKER_NODES" | tail  -1)
 othernodes=$(echo "$WORKER_NODES" | grep -v $sctpnode)
 for node in $othernodes ; do 
   oc label $node node-role.kubernetes.io/worker-rt=""
 done
-# create mcp sctp
+# tag last worker as sctp
+oc label $sctpnode node-role.kubernetes.io/worker-sctp=""
+
+# MCP
+# create sctp machineconfigpool
 oc create -f mcp_sctp.yml
+# create worker-rt machineconfigpool
+# oc create -f mcp_rt.yml
+
+# DEPLOY
 git clone https://github.com/openshift-kni/cnf-features-deploy
 cd cnf-features-deploy
 # create our own env structure
@@ -19,13 +37,3 @@ sed -i "s@image:.*@image: registry-proxy.engineering.redhat.com/rh-osbs/performa
 cp performance_profile.patch.yaml feature-configs/$FEATURES_DIR/performance
 # launch deployment
 FEATURES_ENVIRONMENT=$FEATURES_DIR make feature-deploy
-
-# deploy ptp
-cd 
-git clone https://github.com/openshift-kni/baremetal-deploy
-cd features/ptp
-cp myvars.example myvars
-bash deploy.sh
-bash test.sh
-# deploy sriov
-#cd ~/baremetal-deploy/features/sriov
