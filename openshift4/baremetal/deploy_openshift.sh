f8dce78800914af41eb336aedbf70e6170f063a1
karmab <karimboumedhel@gmail.com>
2020-01-25 19:56:37 +0100

    auto inject pubkeys,pullsecret and libvirturi in install-config when absent

--- openshift4/baremetal/deploy_openshift.sh
+++ openshift4/baremetal/deploy_openshift.sh
@@ -2,16 +2,31 @@
 
 set -euo pipefail
 
-python /root/ipmi.py off
+PYTHON="python"
+which python3 && PYTHON="python3"
+$PYTHON /root/ipmi.py off
 export HOME=/root
 export KUBECONFIG=/root/ocp/auth/kubeconfig
 export OS_CLOUD=metal3-bootstrap
 export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:{{ tag }}
 mkdir -p ocp/openshift
+if [ "$(grep -q libvirtURI /root/install-config.yaml)" != "0" ] ; then
+  SPACES=$(grep apiVIP /root/install-config.yaml | sed 's/apiVIP.*//' | sed 's/ /\\ /'g)
+  sed -i "/hosts/i${SPACES}libvirtURI: qemu+ssh://root@{{ config_host }}/system" /root/install-config.yaml
+fi
+if [ "$(grep -q pullSecret /root/install-config.yaml)" != "0" ] ; then
+    PULLSECRET=$(cat $HOME/openshift_pull.json | tr -d [:space:])
+    echo -e "pullSecret: |\n  $PULLSECRET" >> /root/install-config.yaml
+fi
+if [ "$(grep -q sshKey /root/install-config.yaml)" != "0" ] ; then
+    SSHKEY=$(cat $HOME/.ssh/id_?sa.pub)
+    echo -e "sshKey: |\n  $SSHKEY" >> /root/install-config.yaml
+fi
+
 cp install-config.yaml ocp
 openshift-baremetal-install --dir ocp --log-level debug create manifests
 cp metal3-config.yaml ocp/openshift/99_metal3-config.yaml
-cp manifests/*.{yml,yaml} ocp/openshift/
+cp manifests/*.yaml ocp/openshift/
 openshift-baremetal-install --dir ocp --log-level debug create cluster
 openshift-baremetal-install --dir ocp --log-level debug wait-for install-complete
 openshift-baremetal-install --dir ocp --log-level debug wait-for install-complete
