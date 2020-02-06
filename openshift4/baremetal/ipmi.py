3a38dccf61c400b17e05bda290a5ffe9fc509b12
karmab <karimboumedhel@gmail.com>
2020-02-06 08:33:36 +0100

    fix ipmi.py for ipv6 idracs

--- openshift4/baremetal/ipmi.py
+++ openshift4/baremetal/ipmi.py
@@ -11,7 +11,7 @@ with open(installfile) as f:
     hosts = data['platform']['baremetal']['hosts']
     for host in hosts:
         name = host['name']
-        address = host['bmc']['address'].replace('ipmi://', '')
+        address = host['bmc']['address'].replace('ipmi://', '').replace('[', '').replace(']', '')
         if ':' in address:
             address, port = address.split(':')
             port = '-p %s' % port
