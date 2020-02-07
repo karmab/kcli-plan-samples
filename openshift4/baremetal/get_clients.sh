e89c8be50243951a79863e5aaebc2be1e36e1f60
karmab <karimboumedhel@gmail.com>
2020-02-07 15:48:17 +0100

    use 4.4 installer

--- openshift4/baremetal/get_clients.sh
+++ openshift4/baremetal/get_clients.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --silent https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz > oc.tar.gz
+curl --silent https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.4/linux/oc.tar.gz > oc.tar.gz
 tar zxf oc.tar.gz
 rm -rf oc.tar.gz
 chmod +x oc
