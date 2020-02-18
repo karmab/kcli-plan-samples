254178fc9120224014decadae1ee3489c96185a9
karmab <karimboumedhel@gmail.com>
2020-02-18 15:29:49 +0100

    download oc in /tmp

--- openshift4/baremetal/get_installer.sh
+++ openshift4/baremetal/get_installer.sh
@@ -2,5 +2,6 @@
 
 export PULL_SECRET="/root/openshift_pull.json"
 export OPENSHIFT_RELEASE_IMAGE={{ openshift_image }}
-oc adm release extract --registry-config $PULL_SECRET --command=oc --to . $OPENSHIFT_RELEASE_IMAGE
+oc adm release extract --registry-config $PULL_SECRET --command=oc --to /tmp $OPENSHIFT_RELEASE_IMAGE
+mv /tmp/oc .
 oc adm release extract --registry-config $PULL_SECRET --command=openshift-baremetal-install --to . $OPENSHIFT_RELEASE_IMAGE
