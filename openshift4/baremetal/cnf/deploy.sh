5c6b3cc541f1ded3d4a8eb0f6d7e529f1bc2128a
karmab <karimboumedhel@gmail.com>
2020-02-29 02:11:12 +0100

    openshift: simplified cnf procedure

--- openshift4/baremetal/cnf/deploy.sh
+++ openshift4/baremetal/cnf/deploy.sh
@@ -3,9 +3,6 @@
 #set -euo pipefail
 
 target="u08"
-export HOME=/root
-git config --global http.sslVerify false
-git clone https://gitlab.cee.redhat.com/sysdeseng/cnf-integration.git
 git clone https://github.com/openshift-kni/cnf-features-deploy.git
 cd cnf-features-deploy
-FEATURES_ENVIRONMENT=../../cnf-integration/cnf-kustomize/$target FEATURES="performance ptp sctp dpdk sriov" make setup-test-cluster feature-deploy
+FEATURES_ENVIRONMENT=$target FEATURES="performance ptp sctp dpdk sriov" make setup-test-cluster feature-deploy
