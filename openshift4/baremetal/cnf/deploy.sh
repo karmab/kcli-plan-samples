83dc4a6acffdb961f760a926a61b12068e412603
karmab <karimboumedhel@gmail.com>
2020-03-16 10:13:58 +0100

    support livecd pxe booting

--- openshift4/baremetal/cnf/deploy.sh
+++ openshift4/baremetal/cnf/deploy.sh
@@ -2,7 +2,9 @@
 
 #set -euo pipefail
 
+yum -y install git
 target="integration-u08"
-git clone https://github.com/openshift-kni/cnf-features-deploy.git
+#git clone https://github.com/openshift-kni/cnf-features-deploy.git
+git clone https://code.engineering.redhat.com/gerrit/cnf-internal-deploy.git
 cd cnf-features-deploy
-FEATURES_ENVIRONMENT=$target FEATURES="performance ptp sctp dpdk sriov" make setup-test-cluster feature-deploy
+#FEATURES_ENVIRONMENT=$target FEATURES="performance ptp sctp dpdk sriov" make setup-test-cluster feature-deploy
