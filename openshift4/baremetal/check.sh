a830e7e23d032a56bb5518e714606989759a226a
karmab <karimboumedhel@gmail.com>
2020-03-18 11:25:27 +0100

    Shutdown baremetal plan, to use it internally :(

--- openshift4/baremetal/check.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export KUBECONFIG=/root/ocp/auth/kubeconfig
-echo "Cluster info:"
-oc get clusterversion
-echo "Nodes info:"
-oc get nodes
-echo "Pods info:"
-oc get pod -A
--- openshift4/baremetal/get_installer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo 35.196.103.194 registry.svc.ci.openshift.org >> /etc/hosts
-export PULL_SECRET="/root/openshift_pull.json"
-export OPENSHIFT_RELEASE_IMAGE={{ openshift_image }}
-oc adm release extract --registry-config $PULL_SECRET --command=oc --to /tmp $OPENSHIFT_RELEASE_IMAGE
-mv /tmp/oc .
-oc adm release extract --registry-config $PULL_SECRET --command=openshift-baremetal-install --to . $OPENSHIFT_RELEASE_IMAGE
