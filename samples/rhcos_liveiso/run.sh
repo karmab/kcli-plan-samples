44c3f2e9d3786f425561dbf0ea8cbeab321d7f28
karmab <karimboumedhel@gmail.com>
2020-09-21 11:09:45 +0200

    modifs

--- samples/rhcos_liveiso/run.sh
+++ samples/rhcos_liveiso/run.sh
@@ -1,6 +1,7 @@
-rm -rf source.ign
-kcli create asset -f source.yml > source.ign
+# rm -rf source.ign
+# kcli create asset -f source.yml > source.ign
+kcli create asset -i rhcos46 -P scripts=[source.sh] > source.ign
 alias coreos-installer='podman run --privileged --rm -v /dev:/dev -v /run/udev:/run/udev -v $PWD:/data -w /data quay.io/coreos/coreos-installer:release'
 curl https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/pre-release/latest-4.6/rhcos-live.x86_64.iso > rhcos-live.x86_64.iso
 coreos-installer iso ignition embed -i source.ign rhcos-live.x86_64.iso
-#kcli create vm -P iso=klive.iso
+#kcli create vm -P iso=klive.iso -P nets=[baremetal]
